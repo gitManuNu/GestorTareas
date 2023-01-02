d3a5cfb4c1f2a89ba8f90c349052bafb9854abb7
MANUEL NUÑEZ <manoplamn@gmail.com>
2023-01-02 17:31:57 -0300

    Commit Inicial

---
+++ INSTALADOR/INSTALADOR.bat
@@ -0,0 +1,11 @@
+MKDIR C:\Python\Terminados\Tareas
+pause
+@echo off 
+SET/p carpeta=Current Directoty:
+@echo on
+pause
+"C:\Program Files\WinRAR\WinRAR.exe" x "%carpeta%\INSTALADOR\instalacion.zip" "C:\Python\Terminados\Tareas"
+CD C:\Python\Terminados\Tareas
+copy Mensaje.exe C:\ProgramData\Microsoft\Windows\"Start Menu"\Programs\StartUp
+DEL Mensaje.exe
+pause
